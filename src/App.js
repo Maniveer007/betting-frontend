ca92a9821e6f743904c2295152ed375f2aecf496
Maniveer007 <maniveer198@gmail.com>
2024-05-17 13:06:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import abi from './Betting.json'
+
+function App() {
+  
+  const [Guilds,setGuilds]=useState([]);
+  const [contract,setContract]=useState(null);
+  const [open,setOpen]=useState(false);
+
+  async function loadData(){
+    const provider=new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer=provider.getSigner();
+
+    const address= await signer.getAddress();
+
+    const contract=new ethers.Contract("0x1bafEd069eD4e3a30341A937D3Aae33c5854eb9C",abi,signer);
+    let index=0;
+    let loadguilds=true;
+    let guilds=[];
+    setContract(contract);
+    while(loadguilds){
+      try{
+        const guildsindex=await contract.Guilds(index++);
+        console.log(guildsindex);
+        guilds.push(guildsindex);
+      }catch(e){
+        loadguilds=false;
+      }
+    }
+    setGuilds(guilds);
+  }
+
+  async function handlesubmit(){
+    const guildid=document.getElementById('guild-id').value;
+    const amount=document.getElementById('amount').value;
+    const tx=await contract.placeBidOnGuild(guildid,amount);
+    await tx.wait();
+  
+  }
+  
+  useEffect(()=>{
+    loadData();
+  },[])
+  return (<>
+  <div className='container'> 
+    {Guilds.map((guild,index)=>{
+      return <div onClick={()=>{setOpen(!open)}} className="box" key={index}>{"GuildID:"+Number(guild[0])+"\n \nGuildname:"+guild[1]}</div>
+    })}
+    </div>
+
+    
+        {open && 
+           <div className="form-container">
+            <input type="number" id="guild-id" placeholder="enter guild id" />
+            <input type="number" id="amount" placeholder="enter amount" />
+            <button onClick={handlesubmit}>Placebet</button>
+        </div>
+        }
+
+        </>);
+
+
+}
+
+export default App;
